66e53b993fe3d0d1437875f0eca85335b07bb999
Dusan <dusan.trtica@hotmail.com>
2020-01-23 22:42:06 +0100

    Lesson 4, max counters

--- lesson04/max-counters.js
+++ lesson04/max-counters.js
@@ -2,28 +2,28 @@ const maxCounters = (N, A) => {
     const result = [];
     var maxCounter = 0;
     var lastResetValue = 0;
-    for(var i = 0; i < N; i++) {
+    for (var i = 0; i < N; i++) {
         result[i] = 0;
     }
     const n = A.length;
     for (var i = 0; i < n; i++) {
         const curr = A[i];
-        if (curr <= N) {   
-            if(result[curr-1] < lastResetValue) {
-                result[curr-1] = lastResetValue;
+        if (curr <= N) {
+            if (result[curr - 1] < lastResetValue) {
+                result[curr - 1] = lastResetValue;
             }
-            result[curr-1] +=1;
-                        
-            if(result[curr-1] > maxCounter) {
-                maxCounter = result[curr-1];
+            result[curr - 1] += 1;
+
+            if (result[curr - 1] > maxCounter) {
+                maxCounter = result[curr - 1];
             }
         } else {
             lastResetValue = maxCounter;
         }
     }
 
-    for(var i = 0; i < N; i++) {
-        if(result[i] < lastResetValue) {
+    for (var i = 0; i < N; i++) {
+        if (result[i] < lastResetValue) {
             result[i] = lastResetValue;
         }
     }
