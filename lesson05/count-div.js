310c26028c8c75319c67321794c88ec2a43ea72a
Dusan <dusan.trtica@hotmail.com>
2020-02-25 16:28:09 +0100

    solution

--- lesson05/count-div.js
+++ lesson05/count-div.js
@@ -1,2 +1,37 @@
-export const countDiv = (a, b, k) => {
-}
+export const bruteForceCountDiv = (a, b, k) => {
+  let divisableElemsCount = 0;
+  for (let i = a; i <= b; i++) {
+    if (i % k === 0) {
+      divisableElemsCount += 1;
+    }
+  }
+
+  return divisableElemsCount;
+};
+
+export const countDivKSteps = (a, b, k) => {
+  let divisableElemsCount = 0;
+  let start;
+  if (a % k === 0) {
+    start = a;
+  } else {
+    start = a + Math.abs(a - k);
+  }
+  while (start <= b) {
+    divisableElemsCount += 1;
+    start += k;
+  }
+
+  return divisableElemsCount;
+};
+
+export const countDivConstant = (a, b, k) => {
+  let numDivisorsToB = Math.floor(b / k);
+  const numDivisorsToA = a > 0 ? Math.floor((a - 1) / k) : 0;
+  if (a === 0) {
+    numDivisorsToB += 1;
+  }
+  return numDivisorsToB - numDivisorsToA;
+};
+
+export const countDiv = countDivConstant;
--- lesson05/count-div.test.js
+++ lesson05/count-div.test.js
@@ -1,45 +1,45 @@
 import { countDiv } from './count-div';
 
 describe('countDiv', () => {
-    it('test case 1', () => {
-        expect(countDiv(6, 11, 2)).toBe(3);
-    });
-    it('test case 2', () => {
-        expect(countDiv(2, 10, 1)).toBe(9);
-    })
-    it('test case 3', () => {
-        expect(countDiv(2, 10, 2)).toBe(5);
-    })
-    it('test case 4', () => {
-        expect(countDiv(2, 10, 3)).toBe(3);
-    })
-    it('test case 5', () => {
-        expect(countDiv(1, 1, 1)).toBe(1);
-    })
-    it('test case 6', () => {
-        expect(countDiv(1, 1, 2)).toBe(0);
-    })
-    it('test case 7', () => {
-        expect(countDiv(1, 10, 10)).toBe(1);
-    })
-    it('test case 8', () => {
-        expect(countDiv(1, 9, 10)).toBe(0);
-    });
-    it('test case 9', () => {
-        expect(countDiv(11, 345, 17)).toBe(20);
-    });
-    it('test case 10', () => {
-        expect(countDiv(0, 0, 11)).toBe(1);
-        expect(countDiv(1, 1, 11)).toBe(1);
-    });
-    it('test case 11', () => {
-        expect(countDiv(10, 10, 5)).toBe(0);
-    });
-    it('test case 12', () => {
-        expect(countDiv(10, 10, 7)).toBe(0);
-        expect(countDiv(10, 10, 20)).toBe(0);
-    });
-    it('test case 13', () => {
-        expect(countDiv(11, 13, 12)).toBe(1);
-    })
-})
+  it('test case 1', () => {
+    expect(countDiv(6, 11, 2)).toBe(3);
+  });
+  it('test case 2', () => {
+    expect(countDiv(2, 10, 1)).toBe(9);
+  });
+  it('test case 3', () => {
+    expect(countDiv(2, 10, 2)).toBe(5);
+  });
+  it('test case 4', () => {
+    expect(countDiv(2, 10, 3)).toBe(3);
+  });
+  it('test case 5', () => {
+    expect(countDiv(1, 1, 1)).toBe(1);
+  });
+  it('test case 6', () => {
+    expect(countDiv(1, 1, 2)).toBe(0);
+  });
+  it('test case 7', () => {
+    expect(countDiv(1, 10, 10)).toBe(1);
+  });
+  it('test case 8', () => {
+    expect(countDiv(1, 9, 10)).toBe(0);
+  });
+  it('test case 9', () => {
+    expect(countDiv(11, 345, 17)).toBe(20);
+  });
+  it('test case 10', () => {
+    expect(countDiv(0, 0, 11)).toBe(1);
+    expect(countDiv(1, 1, 11)).toBe(0);
+  });
+  it('test case 11', () => {
+    expect(countDiv(10, 10, 5)).toBe(1);
+  });
+  it('test case 12', () => {
+    expect(countDiv(10, 10, 7)).toBe(0);
+    expect(countDiv(10, 10, 20)).toBe(0);
+  });
+  it('test case 13', () => {
+    expect(countDiv(11, 13, 12)).toBe(1);
+  });
+});
