0f706f6801f7953ab9c3bdc28f7bb8d9cce184b5
Dusan <dusan.trtica@hotmail.com>
2020-04-05 00:12:39 +0200

    added 283, move zeroes task

--- leetcode/283-move-zeroes.test.js
+++ leetcode/283-move-zeroes.test.js
@@ -1,4 +1,33 @@
+const swap = (A, i, j) => {
+  const tmp = A[i];
+  A[i] = A[j];
+  A[j] = tmp;
+};
+
 const moveZeroes = (A) => {
+  const n = A.length;
+  let nonZeroIdx = 0;
+  let zeroIdx = 0;
+
+  while (nonZeroIdx < n) {
+    while (A[nonZeroIdx] === 0) {
+      nonZeroIdx += 1;
+    }
+    while (A[zeroIdx]) {
+      zeroIdx += 1;
+    }
+
+    if (nonZeroIdx >= n || zeroIdx >= n) {
+      break;
+    }
+
+    if (nonZeroIdx < zeroIdx) {
+      nonZeroIdx += 1;
+      continue;
+    } else {
+      swap(A, nonZeroIdx, zeroIdx);
+    }
+  }
   return A;
 };
 
@@ -9,5 +38,21 @@ describe('moveZeroes', () => {
 
   it('case 2', () => {
     expect(moveZeroes([2, 1])).toEqual([2, 1]);
+  });
+
+  it('case 3', () => {
+    expect(moveZeroes([1, 0, 1, 0, 1])).toEqual([1, 1, 1, 0, 0]);
+  });
+
+  it('case 4', () => {
+    expect(moveZeroes([1, 1, 1, 0, 0])).toEqual([1, 1, 1, 0, 0]);
+  });
+
+  it('case 5', () => {
+    expect(moveZeroes([0, 0, 0, 1, 1])).toEqual([1, 1, 0, 0, 0]);
+  });
+
+  it('case 6', () => {
+    expect(moveZeroes([1, 0, 1])).toEqual([1, 1, 0]);
   });
 });
