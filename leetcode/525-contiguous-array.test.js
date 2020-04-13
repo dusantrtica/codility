8f5bd3030739fde5a710b6186b6a5270865cfda9
Dusan <dusan.trtica@hotmail.com>
2020-04-13 18:52:47 +0200

    525 task

---
+++ leetcode/525-contiguous-array.test.js
@@ -0,0 +1,11 @@
+const findMaxLength = (nums) => {};
+
+describe('findMaxLength', () => {
+  it('case 1', () => {
+    expect(findMaxLength([0, 1])).toBe(2);
+  });
+
+  it('case 2', () => {
+    expect(findMaxLength([0, 1, 0])).toBe(2);
+  });
+});
