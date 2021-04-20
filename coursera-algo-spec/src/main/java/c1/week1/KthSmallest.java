75087ee2d90665fd37e0fb6cec278ca448118651
Dusan <dusan.trtica@hotmail.com>
2021-04-21 00:12:19 +0200

    ksmallest wip

---
+++ coursera-algo-spec/src/main/java/c1/week1/KthSmallest.java
@@ -0,0 +1,39 @@
+package c1.week1;
+
+public class KthSmallest {
+    private void swap(int[] A, int i, int j) {
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+
+    private int partition(int[] A, int l, int r) {
+        int p = A[l];
+        int i = l + 1;
+        for (int j = l + 1; j <= r; j++) {
+            if (A[j] < p) {
+                swap(A, i, j);
+                i++;
+            }
+        }
+        swap(A, l, i-1);
+        return i-1;
+    }
+
+    public int kSmallest(int[] A, int low, int high, int k) {
+        if(high == low) {
+            return A[high];
+        }
+
+        int pivotIdx = partition(A, low, high);
+
+        if(pivotIdx - low == k) {
+            return A[pivotIdx-1];
+        }
+
+        if(pivotIdx > k) {
+            return kSmallest(A, low, pivotIdx-1, k);
+        }
+        return kSmallest(A, pivotIdx+1, high, k-pivotIdx - 1);
+    }
+}
---
+++ coursera-algo-spec/src/test/java/c1/week1/KthSmallestSpec.java
@@ -0,0 +1,23 @@
+package c1.week1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class KthSmallestSpec {
+    KthSmallest task = new KthSmallest();
+
+    @Test
+    public void case1() {
+        Assert.assertEquals(2, task.kSmallest(new int []{4,2,1,3}, 0, 3, 2));
+    }
+
+    @Test
+    public void case2() {
+        Assert.assertEquals(1, task.kSmallest(new int []{4,2,1,3,6,5,0}, 0, 6, 2));
+    }
+
+    @Test
+    public void case3() {
+        Assert.assertEquals(3, task.kSmallest(new int []{1,2,3,4,5,6,7,8}, 0, 7, 3));
+    }
+}
