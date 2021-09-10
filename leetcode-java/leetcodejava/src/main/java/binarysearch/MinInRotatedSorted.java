d725664d78f8f85cda4889da5636b42b42f9fd0f
Dusan <dusan.trtica@hotmail.com>
2021-09-11 00:02:24 +0200

    Min in sorted array WIP

---
+++ leetcode-java/leetcodejava/src/main/java/binarysearch/MinInRotatedSorted.java
@@ -0,0 +1,57 @@
+package binarysearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinInRotatedSorted {
+
+  public int findMin(int[] nums) {
+    int n = nums.length;
+    if(n == 1) {
+      return nums[0];
+    }
+
+    int start = 0;
+    int end = n-1;
+    while(start < end) {
+      int mid = (start + end)/2;
+      if(nums[mid-1] > nums[mid] && nums[mid] < nums[mid+1]) {
+        return nums[mid];
+      } else if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
+        start = mid+1;
+      } else {
+        end = mid-1;
+      }
+    }
+    return nums[end];
+  }
+
+  @Test
+  public void case1() {
+    Assert.assertEquals(1, findMin(new int[]{3, 4, 5, 1, 2}));
+  }
+
+  @Test
+  public void case2() {
+    Assert.assertEquals(0, findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
+  }
+
+  @Test
+  public void case3() {
+    Assert.assertEquals(11, findMin(new int[]{11, 13, 15, 17}));
+  }
+
+  @Test
+  public void case4() {
+    Assert.assertEquals(1, findMin(new int []{1}));
+  }
+
+  @Test
+  public void case5() {
+    Assert.assertEquals(1, findMin(new int []{1,2}));
+  }
+  @Test
+  public  void case6() {
+    Assert.assertEquals(1, findMin(new int []{2,1}));
+  }
+}
