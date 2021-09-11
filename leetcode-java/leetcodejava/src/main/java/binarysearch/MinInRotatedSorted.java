29acf2534ddf021d9c1bde4c078460f72854ab3b
Dusan <dusan.trtica@hotmail.com>
2021-09-11 12:47:29 +0200

    Min In Sorted Array Rotaded

--- leetcode-java/leetcodejava/src/main/java/binarysearch/MinInRotatedSorted.java
+++ leetcode-java/leetcodejava/src/main/java/binarysearch/MinInRotatedSorted.java
@@ -15,12 +15,12 @@ public class MinInRotatedSorted {
     int end = n-1;
     while(start < end) {
       int mid = (start + end)/2;
-      if(nums[mid-1] > nums[mid] && nums[mid] < nums[mid+1]) {
-        return nums[mid];
-      } else if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
-        start = mid+1;
+      if(nums[mid] > nums[mid+1]) {
+        return nums[mid+1];
+      } else if (nums[mid] > nums[end]) {
+        start = mid;
       } else {
-        end = mid-1;
+        end = mid;
       }
     }
     return nums[end];
